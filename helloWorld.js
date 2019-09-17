2fa2a75ac15b1b56722c7c038495d3f7b21a8fba
gaolei <leigao@chaindd.com>
2019-09-17 15:33:10 +0800

    add js知识点

--- helloWorld.js
+++ helloWorld.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=> {
+app.use(express.static('public'));
+
+app.get('/hello', (req, res)=> {
 	res.send('\nHello, world!\n')
 })
 
-app.listen(port, () => {
-	console.log(`listening on port ${ port }`)
+var server = app.listen(port, "127.0.0.1", () => {
+	var host = server.address().address
+  	var port = server.address().port
+ 
+  	console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+	// console.log(`listening on port ${ server.address().port } address ${ server.address().address }`)
 })  
